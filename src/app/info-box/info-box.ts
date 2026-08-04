6da4a3d4272ba42a6b8d7fc5941c3c127c725e37
SpexFX <pimo.pk04@gmail.com>
2026-08-04 16:11:04 +0200

    first commit

---
+++ src/app/info-box/info-box.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-box',
+  imports: [],
+  templateUrl: './info-box.html',
+  styleUrl: './info-box.css',
+})
+export class InfoBox {}
